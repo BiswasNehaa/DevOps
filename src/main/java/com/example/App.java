fae62ba428c3a2656754769ebb2f418ae8b6e9d2
BiswasNehaa <neha12345biswas@gmail.com>
2026-02-23 09:44:10 +0530

    DevOps Prog 3

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class App 
+{
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public static void main( String[] args )
+    {
+        App calc= new App();
+        int result = calc.add(5, 3);
+        System.out.println( "Result: " + result );
+    }
+}
